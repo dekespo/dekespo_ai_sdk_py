395eb86265e5f55465ebed19cc1df79774692395
Dekespo <dekespo@gmail.com>
2017-12-18 21:56:08 +0100

    Improving neighbours

--- python/core/neighbours.py
+++ python/core/neighbours.py
@@ -3,16 +3,31 @@ from shapes import *
 from boundaryChecks import *
 from utils import *
 
-# Split this function more?
-def getNeighbours2D_rectangle_4Sides(map, blockingPositions, position):
+def getNeighbours2D_rectangle_4Sides(position):
+    checkType(position, Dim2D)
+    x, y = position.x, position.y
+    candidates = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+    candidates = Dim2D.listToDim2Ds(candidates)
+    return candidates
+
+def getNeighbours2D_rectangle_8Sides(position):
+    checkType(position, Dim2D)
+    x, y = position.x, position.y
+    candidates = [
+        (x+1, y), (x-1, y), (x, y+1), (x, y-1),
+        (x+1, y+1), (x+1, y-1), (x-1, y+1), (x-1, y-1)
+        ]
+    candidates = Dim2D.listToDim2Ds(candidates)
+    return candidates
+
+# Think more about this
+def getAvailableNeighbours2D_rectangle_4Sides(map, blockingPositions, position):
     checkType(map, Rectangle)
     checkType(blockingPositions, list)
     if blockingPositions:
         checkType(blockingPositions[0], Dim2D)
     checkType(position, Dim2D)
-    x, y = position.x, position.y
-    candidates = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
-    candidates = Dim2D.listToDim2Ds(candidates)
+    candidates = getNeighbours2D_rectangle_4Sides(position)
 
     for candi in reversed(candidates):
         isInsideBoundary = boundaryChecks2D_rectangle(map, candi)
@@ -23,8 +38,8 @@ def getNeighbours2D_rectangle_4Sides(map, blockingPositions, position):
     
     return candidates
 
-def getNeighbours2D_rectangle_8Sides(position):
+def getNeighbours2D_hexagon(position):
     pass
 
-def getNeighbours2D_hexagon(position):
+def getAvailableNeighbours2D_rectangle_8Sides(position):
     pass
--- python/tests/neighboursTest.py
+++ python/tests/neighboursTest.py
@@ -7,24 +7,46 @@ from shapes import *
 from neighbours import *
 
 class RectangleNeighbours4SidesTest(unittest.TestCase):
-    def test_twoBlocks(self):
+    def test_rectangleGet4Sides(self):
+        pos = Dim2D(1,1)
+        poses = getNeighbours2D_rectangle_4Sides(pos)
+        self.assertEqual(len(poses), 4)
+        self.assertTrue(Dim2D(0, 1) in poses)
+        self.assertTrue(Dim2D(1, 2) in poses)
+        self.assertTrue(Dim2D(2, 1) in poses)
+        self.assertTrue(Dim2D(1, 0) in poses)
+
+    def test_rectangleGet8Sides(self):
+        pos = Dim2D(1,1)
+        poses = getNeighbours2D_rectangle_8Sides(pos)
+        self.assertEqual(len(poses), 8)
+        self.assertTrue(Dim2D(0, 1) in poses)
+        self.assertTrue(Dim2D(1, 2) in poses)
+        self.assertTrue(Dim2D(2, 1) in poses)
+        self.assertTrue(Dim2D(1, 0) in poses)
+        self.assertTrue(Dim2D(0, 0) in poses)
+        self.assertTrue(Dim2D(2, 2) in poses)
+        self.assertTrue(Dim2D(0, 2) in poses)
+        self.assertTrue(Dim2D(2, 0) in poses)
+
+    def test_availableRectangleTwoBlocks(self):
         map = Rectangle(Dim2D(0, 0), 3, 3)
         blockingPositions = Dim2D.listToDim2Ds([(1,1), (0, 1)])
         pos1 = Dim2D(0, 2)
-        availablePoses = getNeighbours2D_rectangle_4Sides(map, blockingPositions, pos1)
+        availablePoses = getAvailableNeighbours2D_rectangle_4Sides(map, blockingPositions, pos1)
         self.assertEqual(len(availablePoses), 1)
         self.assertEqual(availablePoses[0], Dim2D(1, 2))
         pos2 = Dim2D(1, 2)
-        availablePoses = getNeighbours2D_rectangle_4Sides(map, blockingPositions, pos2)
+        availablePoses = getAvailableNeighbours2D_rectangle_4Sides(map, blockingPositions, pos2)
         self.assertEqual(len(availablePoses), 2)
         self.assertTrue(Dim2D(0, 2) in availablePoses)
         self.assertTrue(Dim2D(2, 2) in availablePoses)
 
-    def test_emptyMap(self):
+    def test_availableRectangleEmptyMap(self):
         map = Rectangle(Dim2D(0, 0), 3, 3)
         blockingPositions = Dim2D.listToDim2Ds([])
         pos1 = Dim2D(1, 1)
-        availablePoses = getNeighbours2D_rectangle_4Sides(map, blockingPositions, pos1)
+        availablePoses = getAvailableNeighbours2D_rectangle_4Sides(map, blockingPositions, pos1)
         self.assertEqual(len(availablePoses), 4)
         self.assertTrue(Dim2D(0, 1) in availablePoses)
         self.assertTrue(Dim2D(1, 2) in availablePoses)
