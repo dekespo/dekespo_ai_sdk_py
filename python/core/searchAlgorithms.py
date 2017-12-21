4a86c5eeba4c0840d43da28b125c7058348f8d32
Dekespo <dekespo@gmail.com>
2017-12-21 21:29:59 +0100

    Added Search algorithm files

---
+++ python/core/searchAlgorithms.py
@@ -0,0 +1,18 @@
+from dimensions import Dim2D
+from shapes import *
+from boundaryChecks import *
+from neighbours import *
+from utils import *
+
+def depthFirstSearch():
+    pass
+
+def breadthFirstSearch():
+    pass
+
+# TODO: might be combined in to AStar search
+def dijkstraSearch():
+    pass
+
+def AStarSearch():
+    pass
---
+++ python/tests/searchAlgorithmsTest.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(0, "core/")
+
+import unittest
+from dimensions import Dim2D
+from shapes import *
+from neighbours import *
+from searchAlgorithms import *
+
+class SearchAlgorithmsTest(unittest.TestCase):
+    def test_depthFirstSearch(self):
+        pass
+
+    def test_breadthFirstSearch(self):
+        pass
+
+    def test_dijkstraSearch(self):
+        pass
+
+    def test_AStarSearch(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
