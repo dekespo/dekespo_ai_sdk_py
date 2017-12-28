9bf20dff9c524eae37ce6433ca4ee642b22abd5a
Dekespo <dekespo@gmail.com>
2017-12-28 18:11:24 +0100

    Added eprint() function for stderr output

--- python/core/utils.py
+++ python/core/utils.py
@@ -5,3 +5,6 @@ def checkType(instance, classType):
 def checkPositiveValue(value):
     if value <= 0:
         raise ArithmeticError("Value " + value + " must be larger than 0")
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
