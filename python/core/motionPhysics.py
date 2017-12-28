7d39a28e574511d300b9173b98de46c1ee8a8452
Dekespo <dekespo@gmail.com>
2017-12-28 17:32:47 +0100

    First motionPhysics files

---
+++ python/core/motionPhysics.py
@@ -0,0 +1,12 @@
+from dimensions import Dim2D
+from utils import *
+
+class MotionPhyiscs:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "pass"
+
+    def __repr__(self):
+        return self.__str__()
---
+++ python/tests/motionPhysicsTest.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(0, "core/")
+
+import unittest
+from shapes import *
+
+class MotionPhysicsTest(unittest.TestCase):
+    def test1(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
