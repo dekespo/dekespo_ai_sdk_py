8cf344f706760cca8ff7359fe680d2f7c6978e92
Dekespo <dekespo@gmail.com>
2017-12-28 18:11:50 +0100

    Improved motion phyiscs

--- python/core/motionPhysics.py
+++ python/core/motionPhysics.py
@@ -1,12 +1,39 @@
 from dimensions import Dim2D
 from utils import *
 
-class MotionPhyiscs:
-    def __init__(self):
-        pass
+class MotionPhysics2D:
+    def __init__(self, position, velocity, acceleration = None, force = None, momentum = None, mass = None):
+        checkType(position, Dim2D)
+        self.position = position
+        checkType(velocity, Dim2D)
+        self.velocity = velocity
+        if acceleration:
+            checkType(acceleration, Dim2D)
+            self.acceleration = acceleration
+        if force:
+            checkType(force, Dim2D)
+            self.force = force
+        if momentum:
+            checkType(momentum, Dim2D)
+            self.momentum = momentum
+        if mass:
+            checkPositiveValue(mass)
+            self.mass = mass
 
+# TODO: Add normalise the units for these variables
+# TODO: Add input conditions for which ones should be accessible(for example, if we have mass and acceleration, we don't need force and momentum)
     def __str__(self):
-        return "pass"
+        string = "Position: " + self.position
+        string += "\nVelocity" + self.velocity
+        if self.acceleration:
+            string += "\nAcceleration" + self.acceleration
+        if self.force:
+            string += "\nForce" + self.force
+        if self.momentum:
+            string += "\nMomentum" + self.momentum
+        if self.mass:
+            string += "\nMass" + self.mass
+        return string
 
     def __repr__(self):
         return self.__str__()
--- python/tests/motionPhysicsTest.py
+++ python/tests/motionPhysicsTest.py
@@ -2,11 +2,54 @@ import sys
 sys.path.insert(0, "core/")
 
 import unittest
-from shapes import *
+from dimensions import Dim2D
+from motionPhysics import *
 
 class MotionPhysicsTest(unittest.TestCase):
-    def test1(self):
-        pass
+    def test_velocity(self):
+        pos = Dim2D(3, 5)
+        vel = Dim2D(2, 2)
+        obj = MotionPhysics2D(pos, vel)
+        self.assertEqual(obj.position, Dim2D(3, 5))
+        self.assertEqual(obj.velocity, Dim2D(2, 2))
+
+# TODO: Some of these values should be changed according to the new input conditions
+    def test_all(self):
+        pos = Dim2D(3, 5)
+        vel = Dim2D(2, 2)
+        acc = Dim2D(1, -1)
+        force = Dim2D(-5, 0)
+        momentum = Dim2D(-9, 9)
+        mass = 6.5
+        obj = MotionPhysics2D(pos, vel, acc, force, momentum, mass)
+        self.assertEqual(obj.position, Dim2D(3, 5))
+        self.assertEqual(obj.velocity, Dim2D(2, 2))
+        self.assertEqual(obj.acceleration, Dim2D(1, -1))
+        self.assertEqual(obj.force, Dim2D(-5, 0))
+        self.assertEqual(obj.momentum, Dim2D(-9, 9))
+        self.assertEqual(obj.mass, 6.5)
+
+    def test_random1(self):
+        pos = Dim2D(3, 5)
+        vel = Dim2D(2, 2)
+        acc = Dim2D(1, -1)
+        mass = 6.5
+        obj = MotionPhysics2D(pos, vel, acceleration=acc, mass=mass)
+        self.assertEqual(obj.position, Dim2D(3, 5))
+        self.assertEqual(obj.velocity, Dim2D(2, 2))
+        self.assertEqual(obj.acceleration, Dim2D(1, -1))
+        self.assertEqual(obj.mass, 6.5)
+
+    def test_random2(self):
+        pos = Dim2D(3, 5)
+        vel = Dim2D(2, 2)
+        force = Dim2D(-5, 0)
+        momentum = Dim2D(-9, 9)
+        obj = MotionPhysics2D(pos, vel, force=force, momentum=momentum)
+        self.assertEqual(obj.position, Dim2D(3, 5))
+        self.assertEqual(obj.velocity, Dim2D(2, 2))
+        self.assertEqual(obj.force, Dim2D(-5, 0))
+        self.assertEqual(obj.momentum, Dim2D(-9, 9))
 
 if __name__ == "__main__":
     unittest.main()
