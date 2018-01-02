4a85e87e1c099c110b8dcb8d4d3dbe4ed6908e37
Dekespo <dekespo@gmail.com>
2018-01-02 14:10:12 +0100

    Added player template class

---
+++ python/templates/player.py
@@ -0,0 +1,22 @@
+class Player:
+    def __init__(self, score, rage):
+        self.score = score
+        self.rage = rage
+        self.units = []
+
+    def __str__(self):
+        return "score: " + str(self.score) + "\n" \
+                + "rage: " + str(self.rage) + "\n" \
+                + "units: " + str(self.units) + "\n"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def addUnit(self, unit):
+        checkType(unit, Unit)
+        self.units.append(unit)
+
+    def chooseUnit(self, type):
+        for object in self.units:
+            object.identity.type == type
+            return object
