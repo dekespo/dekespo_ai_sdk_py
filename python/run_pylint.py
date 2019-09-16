73965bb31f1f539eb508e5a3a9a6f730b78dceff
Dekespo <dekespo@gmail.com>
2019-09-16 10:07:18 +0200

    Removed errors by pylint

--- python/run_pylint.py
+++ python/run_pylint.py
@@ -1,14 +1,14 @@
 import sys
 
-import utils
+import ci_utils
 
 def main():
     base_command = "python -m pylint"
     folders = ["core", "templates", "tests", "."]
     commands = [base_command + " " + next for next in folders]
     for command in commands:
-        returncode = utils.run_process(command, "Pylint")
-        if returncode != utils.RETURN_CODE_OK:
+        returncode = ci_utils.run_process(command, "Pylint")
+        if returncode != ci_utils.RETURN_CODE_OK:
             break
     print("Return code is ", returncode)
     sys.exit(returncode)
--- python/run_tests.py
+++ python/run_tests.py
@@ -1,16 +1,16 @@
 import sys
 import os
 
-import utils
+import ci_utils
 
 def main():
     base_command = "python"
     path = "tests/"
-    file_names = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))] 
+    file_names = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
     commands = [base_command + " " + path + next for next in file_names]
     for command in commands:
-        returncode = utils.run_process(command, "Test")
-        if returncode != utils.RETURN_CODE_OK:
+        returncode = ci_utils.run_process(command, "Test")
+        if returncode != ci_utils.RETURN_CODE_OK:
             break
     print("Return code is ", returncode)
     sys.exit(returncode)
