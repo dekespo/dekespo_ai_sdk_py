3dd7ff25a1bd6dcd5b2b573ea1f0b0ecac8b62d8
Dekespo <dekespo@gmail.com>
2019-12-08 20:44:56 +0100

    Created install or skip depedendencies script instead of using pipfiles

--- continuous_integration/__main__.py
+++ continuous_integration/__main__.py
@@ -3,11 +3,13 @@ import sys
 
 from continuous_integration.run_tests import run_tests
 from continuous_integration.run_pylint import run_pylint
+from continuous_integration.install_or_skip_dependencies import install_or_skip_dependencies
 
 def parse_argurments():
     parser = argparse.ArgumentParser("Continous Integration Tools")
     parser.add_argument("--tests", action='store_true')
     parser.add_argument("--pylint", action='store_true')
+    parser.add_argument("--install_or_skip_dependencies", action='store_true')
     return parser.parse_args()
 
 def main():
@@ -16,6 +18,8 @@ def main():
         returncode = run_tests()
     elif arguments.pylint:
         returncode = run_pylint()
+    elif arguments.install_or_skip_dependencies:
+        returncode = install_or_skip_dependencies()
     print("Return code is ", returncode)
     sys.exit(returncode)
 
