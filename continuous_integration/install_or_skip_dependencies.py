621ce3626bcd4aba0caf7c7fba13a6a2c96932b5
Dekespo <dekespo@gmail.com>
2020-02-15 15:28:08 +0100

    Simplified CI install or skip dependencies file

--- continuous_integration/install_or_skip_dependencies.py
+++ continuous_integration/install_or_skip_dependencies.py
@@ -3,20 +3,16 @@ import sys
 from continuous_integration import utils
 
 def install_or_skip_dependencies():
+    def install_module(command, process_name):
+        returncode = utils.run_process(command, process_name)
+        if returncode != utils.RETURN_CODE_OK:
+            sys.exit(-1)
+        return returncode
+
     pylint_version = "2.3.1"
     coverage_version = "4.5.4"
-    command = "sudo pip install pipenv"
-    returncode = utils.run_process(command, "Pipenv")
-    if returncode != utils.RETURN_CODE_OK:
-        sys.exit(-1)
-    command = f"pipenv install pylint=={pylint_version}"
-    returncode = utils.run_process(command, "Pipenv")
-    if returncode != utils.RETURN_CODE_OK:
-        sys.exit(-1)
-    command = f"pipenv install coverage=={coverage_version}"
-    returncode = utils.run_process(command, "Pipenv")
-    if returncode != utils.RETURN_CODE_OK:
-        sys.exit(-1)
-    command = "pipenv install codecov"
-    returncode = utils.run_process(command, "Pipenv")
+    returncode = install_module("sudo pip install pipenv", "pipenv")
+    returncode = install_module(f"pipenv install pylint=={pylint_version}", "pylint")
+    returncode = install_module(f"pipenv install coverage=={coverage_version}", "coverage")
+    returncode = install_module("pipenv install codecov", "codecov")
     return returncode
