6904147883fa1e737d24b7b048c8fad154afe12c
dekespo <dekespo@gmail.com>
2021-01-16 17:58:46 +0100

    Fix restoring caching on circleci

--- continuous_integration/install_or_skip_dependencies.py
+++ continuous_integration/install_or_skip_dependencies.py
@@ -15,7 +15,7 @@ def install_or_skip_dependencies():
     mypy_version = "0.790"
     pylint_version = "2.6.0"
     xenon_version = "0.7.1"
-    returncode = install_module("sudo pip install pipenv", "pipenv")
+    returncode = install_module("pip install pipenv", "pipenv")
     returncode = install_module(f"pipenv install black=={black_version}", "black")
     returncode = install_module("pipenv install codecov", "codecov")
     returncode = install_module(
@@ -23,5 +23,5 @@ def install_or_skip_dependencies():
     )
     returncode = install_module(f"pipenv install mypy=={mypy_version}", "mypy")
     returncode = install_module(f"pipenv install pylint=={pylint_version}", "pylint")
-    returncode = install_module(f"pipenv install xenon=={xenon_version}", "pylint")
+    returncode = install_module(f"pipenv install xenon=={xenon_version}", "xenon")
     return returncode
